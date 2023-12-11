c7e7d1058ad1c24eba0e6bb87b794c049bd240aa
HyunSeongLee <devhyun05@gmail.com>
2023-12-11 03:14:49 -0500

    comment routing for test

--- backend/server.js
+++ backend/server.js
@@ -29,19 +29,19 @@ app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname + "/public")));
 
 app.get('/', async (req, res) => {
-  try {
+  // try {
     
-    console.log("call");
-    const response = await axios.post('https://api.assemblyai.com/v2/realtime/token',
-      { expires_in: 3600 },
-      { headers: { authorization: process.env.ASSEMBLY_API_KEY } });
-    const { data } = response;
-    console.log(data); 
-    res.json(data);
-  } catch (error) {
-    const { response: { status, data } } = error;
-    res.status(status).json(data);
-  }
+  //   console.log("call");
+  //   const response = await axios.post('https://api.assemblyai.com/v2/realtime/token',
+  //     { expires_in: 3600 },
+  //     { headers: { authorization: process.env.ASSEMBLY_API_KEY } });
+  //   const { data } = response;
+  //   console.log(data); 
+  //   res.json(data);
+  // } catch (error) {
+  //   const { response: { status, data } } = error;
+  //   res.status(status).json(data);
+  // }
 });
 
 app.post("/gptprompt", async (req, res) => {
@@ -61,24 +61,24 @@ app.post("/gptprompt", async (req, res) => {
 })
 
 app.post("/gptvoice", async (req, res) => {
-  try {
+  // try {
 
-    const gptResponse = req.body.prompt; 
+  //   const gptResponse = req.body.prompt; 
 
-    const options = {
-      method: 'POST',
-      headers: {'Accept': 'audio/mpeg', 'Content-Type': 'application/json', "xi-api-key": process.env.ELEVENLABS_API_KEY},
-      body: `{"model_id":"eleven_monolingual_v1","text":"${gptResponse}","voice_settings":{"similarity_boost":0.5,"stability":0.5}}`
-    };
+  //   const options = {
+  //     method: 'POST',
+  //     headers: {'Accept': 'audio/mpeg', 'Content-Type': 'application/json', "xi-api-key": process.env.ELEVENLABS_API_KEY},
+  //     body: `{"model_id":"eleven_monolingual_v1","text":"${gptResponse}","voice_settings":{"similarity_boost":0.5,"stability":0.5}}`
+  //   };
     
-    const response = await fetch('https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM', options);
-    const audioBuffer = await response.arrayBuffer(); 
+  //   const response = await fetch('https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM', options);
+  //   const audioBuffer = await response.arrayBuffer(); 
 
-    res.set('Content-Type', 'audio/mpeg');
-    res.send(Buffer.from(audioBuffer)); 
-  } catch (error) {
-    console.error("Error: ", error);
-  }
+  //   res.set('Content-Type', 'audio/mpeg');
+  //   res.send(Buffer.from(audioBuffer)); 
+  // } catch (error) {
+  //   console.error("Error: ", error);
+  // }
 
 })
 
